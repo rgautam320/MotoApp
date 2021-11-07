ae1ce6db697af90465186ca794f1977465f5d8cf
Rajan Gautam <gautamrajan073@gmail.com>
2021-11-08 01:47:50 +0530

    Fixed Errors

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -16,7 +16,25 @@ export const Handler = (err, req, res, next) => {
 		const message = `Cast Error: Resource Not Found. Invalid: ${err.path}`;
 		err = new ErrorHandler(404, message);
 	}
-	console.log(err);
+
+	// Mongoose Duplicate Key Error
+	if (err.code === 11000) {
+		const message = `Duplicate ${Object.keys(err.keyValue)} Entered`;
+		err = new ErrorHandler(400, message);
+	}
+
+	// Wrong JWT Error
+	if (err.name === "JsonWebTokenError") {
+		const message = `Json Web Token is invalid, Try again `;
+		err = new ErrorHandler(400, message);
+	}
+
+	// JWT EXPIRE Error
+	if (err.name === "TokenExpiredError") {
+		const message = `Json Web Token is Expired, Try again `;
+		err = new ErrorHandler(400, message);
+	}
+
 	res.status(err.statusCode).json({
 		success: false,
 		message: err.message,
