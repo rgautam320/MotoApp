6d5a3c88d2522d62346fa851030e7f65d98622d2
Rajan Gautam <gautamrajan073@gmail.com>
2021-11-08 00:59:25 +0530

    Auth Middleware Added

---
+++ backend/middleware/auth.js
@@ -0,0 +1,27 @@
+import jwt from "jsonwebtoken";
+import User from "../models/userModel.js";
+import Catch from "./catch.js";
+import ErrorHandler from "./error.js";
+
+export const isAuthenticated = Catch(async (req, res, next) => {
+	const { token } = req.cookies;
+
+	if (!token) {
+		return next(new ErrorHandler(401, "Unauthorized: Please Login"));
+	}
+
+	const decodedData = jwt.verify(token, process.env.JWT_SECRET);
+
+	req.user = await User.findById(decodedData.id);
+
+	next();
+});
+
+export const isAdmin = () => {
+	return (req, res, next) => {
+		if (req.user.role === "user") {
+			return next(new ErrorHandler(401, "Unauthorized: You can't access this."));
+		}
+		next();
+	};
+};
