e9dc833c2152c86618dbc5bf8af270ebb4d92cfe
Rajan Gautam <gautamrajan073@gmail.com>
2021-11-08 01:00:06 +0530

    Added Mail Option for Resetting Password

---
+++ backend/utils/sendEmail.js
@@ -0,0 +1,23 @@
+import nodemailer from "nodemailer";
+
+const sendEmail = async (options) => {
+	const transporter = nodemailer.createTransport({
+		host: process.env.SMPT_HOST,
+		port: process.env.SMPT_PORT,
+		service: process.env.SMPT_SERVICE,
+		auth: {
+			user: process.env.SMPT_MAIL,
+			pass: process.env.SMPT_PASSWORD,
+		},
+	});
+	const mailOptions = {
+		from: process.env.SMPT_MAIL,
+		to: options.email,
+		subject: options.subject,
+		message: options.message,
+	};
+
+	await transporter.sendMail(mailOptions);
+};
+
+export default sendEmail;
