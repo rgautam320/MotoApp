23244757c4befe9cfb98c3e8227f07adbe72b897
Rajan Gautam <gautamrajan073@gmail.com>
2021-11-08 00:58:21 +0530

    Error Middleware Updated

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -1,13 +1,20 @@
-import Handler from "../utils/ErrorHandler.js";
+class ErrorHandler extends Error {
+	constructor(statusCode, message) {
+		super(message);
+		this.statusCode = statusCode;
 
-const ErrorHandler = (err, req, res, next) => {
+		Error.captureStackTrace(this, this.constructor);
+	}
+}
+
+export const Handler = (err, req, res, next) => {
 	err.statusCode = err.statusCode || 500;
 	err.message = err.message || "Internal Server Error";
 
 	// Handling MongoDB Error
 	if (err.name === "CastError") {
 		const message = `Cast Error: Resource Not Found. Invalid: ${err.path}`;
-		err = new Handler(404, message);
+		err = new ErrorHandler(404, message);
 	}
 
 	res.status(err.statusCode).json({
