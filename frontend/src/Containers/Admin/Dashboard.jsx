3e1162524b45716df549bef948ef98642492c52a
Rajan Gautam <gautamrajan073@gmail.com>
2021-12-25 15:41:58 +0530

    Added Chart

--- frontend/src/Containers/Admin/Dashboard.jsx
+++ frontend/src/Containers/Admin/Dashboard.jsx
@@ -1,9 +1,19 @@
 import React from "react";
-import { Doughnut, Line } from "react-chartjs-2";
+import { Chart as ChartJS, CategoryScale, LinearScale, ArcElement, PointElement, LineElement, Title, Tooltip, Legend } from "chart.js";
+import { Line, Doughnut } from "react-chartjs-2";
 import { NavLink } from "react-router-dom";
-import Sidebar from "../../Components/Admin/Sidebar";
+
+ChartJS.register(CategoryScale, LinearScale, ArcElement, PointElement, LineElement, Title, Tooltip, Legend);
 
 const Dashboard = () => {
+	const options = {
+		responsive: true,
+		plugins: {
+			legend: {
+				position: "bottom",
+			},
+		},
+	};
 	const lineState = {
 		labels: ["Initial Amount", "Amount Earned"],
 		datasets: [
@@ -15,13 +25,33 @@ const Dashboard = () => {
 			},
 		],
 	};
-	const doughnutState = {
+	const productState = {
 		labels: ["Out of Stock", "InStock"],
 		datasets: [
 			{
 				backgroundColor: ["#00A6B4", "#6800B4"],
 				hoverBackgroundColor: ["#4B5000", "#35014F"],
 				data: [2, 10],
+			},
+		],
+	};
+	const orderState = {
+		labels: ["Delivered", "Not Delivered"],
+		datasets: [
+			{
+				backgroundColor: ["#00A6B4", "#6800B4"],
+				hoverBackgroundColor: ["#4B5000", "#35014F"],
+				data: [8, 6],
+			},
+		],
+	};
+	const userState = {
+		labels: ["Active", "Not Active"],
+		datasets: [
+			{
+				backgroundColor: ["#00A6B4", "#6800B4"],
+				hoverBackgroundColor: ["#4B5000", "#35014F"],
+				data: [16, 2],
 			},
 		],
 	};
@@ -35,21 +65,46 @@ const Dashboard = () => {
 					<NavLink exact to="/admin/dashboard/products" className="adminDashboard__circles__links adminDashboard__circles__link1">
 						<div className="adminDashboard__circles__links__details">
 							<span>Products</span>
-							<span>{10}</span>
+							<span>{12}</span>
 						</div>
 					</NavLink>
 					<NavLink exact to="/admin/dashboard/orders" className="adminDashboard__circles__links adminDashboard__circles__link2">
 						<div className="adminDashboard__circles__links__details">
 							<span>Orders</span>
-							<span>{10}</span>
+							<span>{14}</span>
 						</div>
 					</NavLink>
 					<NavLink exact to="/admin/dashboard/users" className="adminDashboard__circles__links adminDashboard__circles__link3">
 						<div className="adminDashboard__circles__links__details">
 							<span>Users</span>
-							<span>{10}</span>
+							<span>{18}</span>
 						</div>
 					</NavLink>
+				</div>
+				<hr />
+				<div className="adminDashboard__lineChart">
+					<Line options={options} data={lineState} />
+				</div>
+				<hr />
+				<div className="row my-5">
+					<div className="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12">
+						<div className="adminDashboard__doughnutChart">
+							<h5 className="sub-heading">Products</h5>
+							<Doughnut options={options} data={productState} />
+						</div>
+					</div>
+					<div className="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12">
+						<div className="adminDashboard__doughnutChart">
+							<h5 className="sub-heading">Orders</h5>
+							<Doughnut options={options} data={orderState} />
+						</div>
+					</div>
+					<div className="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12">
+						<div className="adminDashboard__doughnutChart">
+							<h5 className="sub-heading">Users</h5>
+							<Doughnut options={options} data={userState} />
+						</div>
+					</div>
 				</div>
 			</div>
 		</>
--- frontend/src/HOCS/AdminLayout.jsx
+++ frontend/src/HOCS/AdminLayout.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import Sidebar from "../Components/Admin/Sidebar";
 import Navbar from "../Components/Navbar/Navbar";
 import AdminRoutes from "./AdminRoutes";
+import MetaData from "./MetaData";
 
 const AdminLayout = () => {
 	return (
 		<>
+			<MetaData title="Moto App | Admin Dashboard" />
 			<Navbar />
 			<div className="container-fluid">
 				<div className="row">
-					<div className="col-xl-2 col-lg-3 col-md-3 col-sm-12 col-12" style={{ paddingTop: "18vh" }}>
+					<div className="col-xl-2 col-lg-3 col-md-3 col-sm-12 col-12" style={{ paddingTop: "15vh" }}>
 						<Sidebar />
 					</div>
 					<div className="col-xl-10 col-lg-9 col-md-9 col-sm-12 col-12">
